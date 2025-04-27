1b474ae69290d0a2e712bb69371211991318e7a9
brijesh <brijeshkumar4705r@gmail.com>
2025-04-27 11:56:37 +0530

    incapsulation (setter&getter)

--- oops.cpp
+++ oops.cpp
@@ -1,66 +1,101 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Employe {
-private:
-    int Age;
-    string Gender;
-    string Name;
 
-public:
-    // Constructor
-    Employe(string name, string gender, int age) {
-        setName(name);
-        setGender(gender);
-        setAge(age);
-    }
+class employe{
 
-    // Setters
-    void setName(string name) {
-        Name = name;
-    }
+   private :
+    
+     int Age; 
+      string Gender ;
+        
+       string Name; 
+        
 
-    void setGender(string gender) {
-        Gender = gender;
-    }
 
-    void setAge(int age) {
-        if (age >= 18) {
-            Age = age;
-        } else {
-            Age = 18; // Set minimum age to 18
-        }
-    }
+       public: 
+    void print(){
+     
+ cout<<"My name is- "<<getname()<<endl<<"age is- "<<getage()<<endl<<"gender - "<<getgender()<< endl;
+  
 
-    // Getters
-    string getName() {
+    }    
+
+
+       void setname( string name){
+
+       Name  = name ; 
+         
+
+       }
+
+       string getname( ){
+
         return Name;
+         
+       }
+
+       void setgender( string gender){
+
+        Gender  = gender ; 
+          
+ 
+        }
+ 
+        string getgender( ){
+ 
+         return Gender;
+          
+        }
+
+
+        void setage(int age){
+     
+               if(age >=18){
+
+                Age = age ;
+               }
+               else{
+   
+   Age = 18 ;
+     
+               }
+              
+        }
+
+        int getage(){
+
+            return Age;
+             
+        }
+  
+  
+    
+
+    employe ( string name , string gender  , int age ){
+  
+  Name = name; 
+   Age= age ;
+   Gender = gender;
+          
+
     }
 
-    string getGender() {
-        return Gender;
-    }
 
-    int getAge() {
-        return Age;
-    }
 
-    // Print function
-    void print() {
-        cout << "My name is: " << getName() << endl
-             << "Age is: " << getAge() << endl
-             << "Gender: " << getGender() << endl;
-    }
 };
 
-int main() {
-    Employe emp("Brijesh", "Male", 22);
-    emp.print();
+int main (){
 
-    cout << "------------------------" << endl;
+employe emp = employe( "brijesh" , " male" , 22);
+ 
+     emp.print();
+      
 
-    Employe emp2("John", "Male", 10);
-    emp2.print();
+       employe emp2 = employe("jhon " , " male" , 10);
+         
+      emp2. print();
+        
 
-    return 0;
+
 }
