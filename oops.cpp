a4a3c4c900069478d0627d0c0032dcc8247a56df
brijesh <brijeshkumar4705r@gmail.com>
2025-04-27 11:54:37 +0530

    setter and getter

--- oops.cpp
+++ oops.cpp
@@ -1,44 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Employe {
+private:
+    int Age;
+    string Gender;
+    string Name;
 
-class employe{
-
-   public :
-    
-     int Age; 
-      string Gender ;
-        
-       string Name; 
-        
-    void print(){
-     
- cout<<"My name is- "<<Name<<endl<<"age is- "<<Age<<endl<<"gender - "<<Gender;
-  
-
-    }    
-
-    
-
-    employe ( string name , string gender  , int age ){
-  
-  Name = name; 
-   Age= age ;
-   Gender = gender;
-          
-
+public:
+    // Constructor
+    Employe(string name, string gender, int age) {
+        setName(name);
+        setGender(gender);
+        setAge(age);
     }
 
+    // Setters
+    void setName(string name) {
+        Name = name;
+    }
 
+    void setGender(string gender) {
+        Gender = gender;
+    }
 
+    void setAge(int age) {
+        if (age >= 18) {
+            Age = age;
+        } else {
+            Age = 18; // Set minimum age to 18
+        }
+    }
+
+    // Getters
+    string getName() {
+        return Name;
+    }
+
+    string getGender() {
+        return Gender;
+    }
+
+    int getAge() {
+        return Age;
+    }
+
+    // Print function
+    void print() {
+        cout << "My name is: " << getName() << endl
+             << "Age is: " << getAge() << endl
+             << "Gender: " << getGender() << endl;
+    }
 };
 
-int main (){
+int main() {
+    Employe emp("Brijesh", "Male", 22);
+    emp.print();
 
-employe emp = employe( "brijesh" , " male" , 22);
- 
-     emp.print();
-      
+    cout << "------------------------" << endl;
 
+    Employe emp2("John", "Male", 10);
+    emp2.print();
 
+    return 0;
 }
