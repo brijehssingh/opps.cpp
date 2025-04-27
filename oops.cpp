95e78c6cf10326c666132fa330edd16f03cb2e9c
brijesh <brijeshkumar4705r@gmail.com>
2025-04-27 22:12:54 +0530

    pure virtual function

--- oops.cpp
+++ oops.cpp
@@ -1,101 +1,83 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class employe{
-
-   private :
-    
-     int Age; 
-      string Gender ;
-        
-       string Name; 
-        
-
-
-       public: 
-    void print(){
-     
- cout<<"My name is- "<<getname()<<endl<<"age is- "<<getage()<<endl<<"gender - "<<getgender()<< endl;
-  
-
-    }    
-
-
-       void setname( string name){
-
-       Name  = name ; 
-         
-
-       }
-
-       string getname( ){
-
-        return Name;
-         
-       }
-
-       void setgender( string gender){
-
-        Gender  = gender ; 
-          
- 
-        }
- 
-        string getgender( ){
- 
-         return Gender;
-          
-        }
-
-
-        void setage(int age){
-     
-               if(age >=18){
-
-                Age = age ;
-               }
-               else{
-   
-   Age = 18 ;
-     
-               }
-              
-        }
-
-        int getage(){
-
-            return Age;
-             
-        }
-  
-  
-    
-
-    employe ( string name , string gender  , int age ){
-  
-  Name = name; 
-   Age= age ;
-   Gender = gender;
-          
-
-    }
-
-
-
+class promotion {
+public:
+    // Pure virtual function
+    virtual void promotionEligible() = 0; 
 };
 
-int main (){
+class employee :  promotion {
+private:
+    int Age; 
+    string Gender;
+    string Name; 
 
-employe emp = employe( "brijesh" , " male" , 22);
- 
-     emp.print();
-      
-
-       employe emp2 = employe("jhon " , " male" , 10);
+public: 
+    // Constructor
+    employee(string name, string gender, int age) {
+        Name = name;
+        Gender = gender;
+        Age =age;
          
-      emp2. print();
-        
+    }
 
+    // Setter and Getter for Name
+    void setname(string name) {
+        Name = name;
+    }
 
+    string getname() {
+        return Name;
+    }
+
+    // Setter and Getter for Gender
+    void setgender(string gender) {
+        Gender = gender;
+    }
+
+    string getgender() {
+        return Gender;
+    }
+
+    // Setter and Getter for Age
+    void setage(int age) {
+        if (age >= 18) {
+            Age = age;
+        } else {
+            Age = 18; // default to 18
+        }
+    }
+
+    int getage() {
+        return Age;
+    }
+
+    // Print function
+    void print() {
+        cout << "My name is: " << getname() 
+             << ", Age: " << getage() 
+             << ", Gender: " << getgender() << endl;
+    }
+
+    // Overriding the pure virtual function
+    void promotionEligible() override {
+        if (Age > 30) {
+            cout << Name << " is eligible for promotion!" << endl;
+        } else {
+            cout << Name << " is NOT eligible for promotion yet." << endl;
+        }
+    }
+};
+
+int main() {
+    employee emp1("Brijesh", "Male", 44);
+    emp1.print();
+    emp1.promotionEligible();
+
+    employee emp2("John", "Male", 56);
+    emp2.print();
+    emp2.promotionEligible();
+
+    return 0;
 }
